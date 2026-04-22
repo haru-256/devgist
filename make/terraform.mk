52d300008bcb0d61bd2972ff0a8f6ce9af74094b
Yohei Okabayashi <yohei.kuro48@gmail.com>
2026-04-22 12:36:25 +0900

    fix(terraform): align make includes

--- make/terraform.mk
+++ make/terraform.mk
@@ -5,6 +5,8 @@ TFLINT_CONFIG ?= $(TERRAFORM_ROOT)/.tflint.hcl
 BACKEND_CONFIG ?= config.gcs.tfbackend
 TRIVY_SEVERITY ?= HIGH,CRITICAL
 
+include $(MAKE_DIR)/help.mk
+
 .PHONY: fmt
 fmt: ## Format terraform recursively
 	terraform fmt -recursive
@@ -12,7 +14,7 @@ fmt: ## Format terraform recursively
 .PHONY: lint
 lint: ## Lint terraform and scan config
 	tflint --recursive --config $(TFLINT_CONFIG)
-	trivy config --severity=$(TRIVY_SEVERITY) --tf-vars=terraform.tfvars ./
+	trivy config --severity=$(TRIVY_SEVERITY) $(if $(wildcard terraform.tfvars),--tf-vars=terraform.tfvars) ./
 
 .PHONY: init
 init: ## Initialize terraform
