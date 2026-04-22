7db15c61897eb027ddba59fcd371f07e73850c62
Yohei Okabayashi <yohei.kuro48@gmail.com>
2026-04-22 12:46:52 +0900

    [codex] Terraform Makefile を共通化する (#64)
    * refactor(terraform): centralize shared makefiles
    
    * fix(terraform): align make includes
    
    * refactor(terraform): separate help include

--- infra/terraform/scripts/common.mk
+++
@@ -1,8 +0,0 @@
-.PHONY: fmt
-fmt: # format terraform
-	terraform fmt -recursive
-
-.PHONY: lint
-lint: # lint terraform
-	tflint --recursive --config $(TFLINT_CONFIG)
-	trivy config --severity=HIGH,CRITICAL --tf-vars=terraform.tfvars ./
